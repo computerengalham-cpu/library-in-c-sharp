43b3003dbfd44dc58d978d5a9024f3d2cac9001c
Alham Saif <computerengalham@gmail.com>
2026-04-22 10:54:15 +0400

    choose the Exam and open it

---
+++ solution9/project1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.IO;
+    namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (FileStream fs = File.Create("test1.txt")) ;
+            //string[] lines1 = { "Ebtisam", "Hayat" };
+            //File.WriteAllLines("test1.txt", lines1);
+            //File.AppendAllText("test1.txt", "\n hello all");
+            //string myfile=File.ReadAllText("test1.txt");
+            //Console.WriteLine(myfile);
+
+
+            Console.WriteLine("please choose Exam1 or Exam2 or Exam3");
+            string user = Console.ReadLine();
+
+            string fileName = "";
+
+            if (user == "1")
+            {
+                fileName = "Exam1.txt";
+            }
+            else if (user == "2")
+            {
+                fileName = "Exam2.txt";
+            }
+            else
+            {
+                fileName = "Exam3.txt";
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = fileName,
+                UseShellExecute = true
+            });
+
+
+
+        }
+    }
+}
